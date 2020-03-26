67d0ee1fd9cdb8650fdb7b193e9f92b474bbc01c
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-03-27 00:52:25 +0800

    初始提交

--- admin-demo/shower-commons-db/src/main/java/com/example/commons/db/pojo/BaseDbPageResult.java
+++ admin-demo/shower-commons-db/src/main/java/com/example/commons/db/pojo/BaseDbPageResult.java
@@ -13,13 +13,13 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class BaseDbPageResult<T> extends BaseEntity {
-    private Long page;
-    private Long size;
-    private Long totalNumberOfRows;
-    private Long totalPages;
+    private long page;
+    private long size;
+    private long totalNumberOfRows;
+    private long totalPages;
     private Iterable<T> data;
 
-    public BaseDbPageResult(Long page, Long size, Long totalNumberOfRows, Long totalPages, Iterable<T> data) {
+    public BaseDbPageResult(long page, long size, long totalNumberOfRows, long totalPages, Iterable<T> data) {
         this.page = page;
         this.size = size;
         this.totalNumberOfRows = totalNumberOfRows;
