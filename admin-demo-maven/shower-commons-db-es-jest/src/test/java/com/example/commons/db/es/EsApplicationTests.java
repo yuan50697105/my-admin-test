f0374c5a505f72cce8b6020a6cca8904414ef462
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-04-01 22:48:38 +0800

    maven gradle 双版本同步,修改Redis

---
+++ admin-demo-maven/shower-commons-db-es-jest/src/test/java/com/example/commons/db/es/EsApplicationTests.java
@@ -0,0 +1,31 @@
+package com.example.commons.db.es;
+
+import com.example.commons.db.es.test.SysUser;
+import com.example.commons.db.es.test.SysUserRepo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootTest(classes = EsApplication.class)
+public class EsApplicationTests {
+    @Autowired
+    private ApplicationContext context;
+
+    @Autowired
+    private SysUserRepo sysUserRepo;
+
+    @Test
+    void name() {
+        SysUser sysUser = SysUser.builder().content("asdasd").build();
+        SysUser save = sysUserRepo.save(sysUser);
+        System.out.println("save = " + save);
+
+    }
+
+    @Test
+    void t2() {
+        Iterable<SysUser> sysUsers = sysUserRepo.findAll();
+        sysUsers.forEach(sysUser -> System.out.println("sysUser = " + sysUser));
+    }
+}
