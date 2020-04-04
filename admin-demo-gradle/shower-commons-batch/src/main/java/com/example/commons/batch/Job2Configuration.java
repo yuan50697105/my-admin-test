0e3c1bb332b62fe217acd890e2fd17c70b3e8d87
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-04-04 17:30:22 +0800

    QuartzBatchApplication

--- admin-demo-gradle/shower-commons-batch/src/main/java/com/example/commons/batch/Job2Configuration.java
+++ admin-demo-gradle/shower-commons-batch/src/main/java/com/example/commons/batch/Job2Configuration.java
@@ -2,15 +2,11 @@ package com.example.commons.batch;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.job.builder.FlowBuilder;
 import org.springframework.batch.core.job.flow.Flow;
-import org.springframework.batch.core.job.flow.FlowExecutionStatus;
-import org.springframework.batch.core.job.flow.JobExecutionDecider;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -27,6 +23,8 @@ import org.springframework.context.annotation.Configuration;
 public class Job2Configuration {
     public static final String JOB_02 = "job_02";
     public static final String JOB_02_STEP_01 = "job_02_step_01";
+    public static final String JOB_02_STEP_02 = "job_02_step_02";
+    public static final String FLOW_01 = "flow_01";
     @Autowired
     private JobBuilderFactory jobBuilderFactory;
     @Autowired
@@ -34,27 +32,28 @@ public class Job2Configuration {
 
     @Bean(JOB_02)
     public Job job02() {
-        return jobBuilderFactory.get(JOB_02).start(flow()).build().build();
+        return jobBuilderFactory.get(JOB_02+"_"+System.currentTimeMillis()).start(flow()).end().build();
     }
 
+    @Bean(FLOW_01)
     public Flow flow() {
-        FlowBuilder<Flow> flow_01 = new FlowBuilder<>("flow_01");
+        FlowBuilder<Flow> flow_01 = new FlowBuilder<>(FLOW_01);
         flow_01.start(job02Step01());
-        flow_01.from(flowDecider()).on(FlowExecutionStatus.COMPLETED.getName());
+        flow_01.next(job02Step02());
         return flow_01.build();
-    }
-
-    private JobExecutionDecider flowDecider() {
-        return new JobExecutionDecider() {
-            @Override
-            public FlowExecutionStatus decide(JobExecution jobExecution, StepExecution stepExecution) {
-                return FlowExecutionStatus.COMPLETED;
-            }
-        };
     }
 
     @Bean(JOB_02_STEP_01)
     public Step job02Step01() {
+        return stepBuilderFactory.get(JOB_02_STEP_01).tasklet((contribution, chunkContext) -> {
+            chunkContext.setAttribute("abc", "abc");
+            log.info("aaaaaaaaaa");
+            return RepeatStatus.FINISHED;
+        }).build();
+    }
+
+    @Bean(JOB_02_STEP_02)
+    public Step job02Step02() {
         return stepBuilderFactory.get(JOB_02_STEP_01).tasklet((contribution, chunkContext) -> {
             chunkContext.setAttribute("abc", "abc");
             log.info("aaaaaaaaaa");
