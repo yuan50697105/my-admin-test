5324251046ef3e120ee9a6e2c5259a633c50450f
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-04-04 21:07:17 +0800

    QuartzBatchApplication

--- admin-demo-gradle/shower-commons-batch/src/main/java/com/example/commons/batch/BatchApplication.java
+++ admin-demo-gradle/shower-commons-batch/src/main/java/com/example/commons/batch/BatchApplication.java
@@ -1,7 +1,10 @@
 package com.example.commons.batch;
 
+import cn.hutool.extra.spring.SpringUtil;
+import org.springframework.batch.core.Job;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 /**
  * @program: admin-demo-gradle
@@ -10,8 +13,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @create: 2020-04-04 15:03
  */
 @SpringBootApplication
+@Import(SpringUtil.class)
 public class BatchApplication {
     public static void main(String[] args) {
+        Job job = SpringUtil.getBean(Job1Configuration.JOB_01, Job.class);
+
         SpringApplication.run(BatchApplication.class, args);
     }
 }
