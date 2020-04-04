28cbccf32425f7db6ab2b314019b648e944ab357
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-04-04 15:31:04 +0800

    QuartzBatchApplication

---
+++ admin-demo-gradle/shower-commons-batch-quartz/src/main/java/com/example/commons/batch/QuartzBatchApplication.java
@@ -0,0 +1,17 @@
+package com.example.commons.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: admin-demo-gradle
+ * @description:
+ * @author: yuane
+ * @create: 2020-04-04 15:03
+ */
+@SpringBootApplication
+public class QuartzBatchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QuartzBatchApplication.class, args);
+    }
+}
