5da7864bdbc22ca966c3b78beade5bedda34766a
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-04-05 12:18:24 +0800

    Spring Data Redis

--- admin-demo-gradle/shower-commons-db-es-jest/src/main/java/com/example/commons/db/es/test/SysUser.java
+++ admin-demo-gradle/shower-commons-db-es-jest/src/main/java/com/example/commons/db/es/test/SysUser.java
@@ -7,21 +7,20 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
-import org.springframework.data.elasticsearch.annotations.FieldType;
 
-@Document(indexName = "sys_user", type = "article")
+@Document(indexName = "sys_user", type = "article", useServerConfiguration = true)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class SysUser {
     @Id
-    @Field(type = FieldType.Long, index = true, store = true)//index 默认为true，可以不写
+    @Field(index = true, store = true)//index 默认为true，可以不写
     private long id;
 
-    @Field(type = FieldType.Text, index = true, store = true, analyzer = "ik_smart", searchAnalyzer = "ik_max_word")
+    @Field(index = true, store = true, analyzer = "ik_pinyin_analyzer", searchAnalyzer = "ik_pinyin_analyzer")
     private String title;
 
-    @Field(type = FieldType.Text, index = true, store = true, analyzer = "ik_smart", searchAnalyzer = "ik_max_word")
+    @Field(index = true, store = true, analyzer = "ik_pinyin_analyzer", searchAnalyzer = "ik_pinyin_analyzer")
     private String content;
 }
