bf8a86dd76039736ab1479334715ff287d535f7c
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-04-06 18:02:08 +0800

    测试日期处理

---
+++ admin-demo-gradle/shower-commons-db-mybatis-base-with-pagehelper/src/main/java/com/example/commons/db/mybatis/pagehelper/test/service/impl/SysRoleService.java
@@ -0,0 +1,31 @@
+package com.example.commons.db.mybatis.pagehelper.test.service.impl;
+
+import java.util.List;
+import com.example.commons.db.mybatis.pagehelper.test.pojo.SysRoleExample;
+import com.example.commons.db.mybatis.pagehelper.test.pojo.SysRole;
+public interface SysRoleService{
+
+
+    long countByExample(SysRoleExample example);
+
+    int deleteByExample(SysRoleExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(SysRole record);
+
+    int insertSelective(SysRole record);
+
+    List<SysRole> selectByExample(SysRoleExample example);
+
+    SysRole selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(SysRole record,SysRoleExample example);
+
+    int updateByExample(SysRole record,SysRoleExample example);
+
+    int updateByPrimaryKeySelective(SysRole record);
+
+    int updateByPrimaryKey(SysRole record);
+
+}
