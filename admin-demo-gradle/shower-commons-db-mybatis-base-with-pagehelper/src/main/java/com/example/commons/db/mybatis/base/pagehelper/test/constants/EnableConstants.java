30ff25cbd343978988115bf8b2b933c751839eab
yuanenzhi <710575900@qq.com>
2020-04-09 16:55:40 +0800

    app01 添加userRole

--- admin-demo-gradle/shower-commons-db-mybatis-base-with-pagehelper/src/main/java/com/example/commons/db/mybatis/base/pagehelper/test/constants/EnableConstants.java
+++ admin-demo-gradle/shower-commons-db-mybatis-base-with-pagehelper/src/main/java/com/example/commons/db/mybatis/base/pagehelper/test/constants/EnableConstants.java
@@ -2,8 +2,6 @@ package com.example.commons.db.mybatis.base.pagehelper.test.constants;
 
 import com.example.commons.db.utils.DictUtils;
 import com.example.commons.db.utils.annotation.ConstantsName;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
@@ -13,23 +11,10 @@ import java.util.Map;
  * @author: yuane
  * @create: 2020-04-06 02:47
  */
-@Component(EnableConstants.ENABLE_CONSTANTS_RUNNER)
-public class EnableConstants implements CommandLineRunner {
-    public static final String ENABLE_CONSTANTS_RUNNER = "enableConstantsRunner";
+
+public class EnableConstants {
     @ConstantsName(name = "启用")
     public static final String ENABLED = "ENABLED";
     @ConstantsName(name = "停用")
     public static final String DISABLED = "DISABLED";
-
-    private EnableConstants() {
-
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        Map<String, String> dictKeyValue = DictUtils.getDictKeyValue(this.getClass());
-        String dictCode = DictUtils.getDictCode(this.getClass());
-
-    }
-
 }
