b2ac02f6c30b2401934d35b559d1aac8be884a69
yuanenzhi <710575900@qq.com>
2020-04-09 11:08:49 +0800

    修改路径

---
+++ admin-demo-gradle/shower-application-08/src/main/java/com/example/Showerapplication08.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: admin-demo-gradle
+ * @description:
+ * @author: yuane
+ * @create: 2020-04-06 13:44
+ */
+@SpringBootApplication
+public class Showerapplication08 {
+    public static void main(String[] args) {
+        SpringApplication.run(Showerapplication08.class, args);
+    }
+}
