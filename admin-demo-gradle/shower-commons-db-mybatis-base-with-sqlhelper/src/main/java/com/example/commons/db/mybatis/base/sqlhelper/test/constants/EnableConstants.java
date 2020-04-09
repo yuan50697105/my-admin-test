b4278f044127fb735ec79f4a2a61f773a4ecc33b
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-04-09 21:37:11 +0800

    i

--- admin-demo-gradle/shower-commons-db-mybatis-base-with-sqlhelper/src/main/java/com/example/commons/db/mybatis/base/sqlhelper/test/constants/EnableConstants.java
+++ admin-demo-gradle/shower-commons-db-mybatis-base-with-sqlhelper/src/main/java/com/example/commons/db/mybatis/base/sqlhelper/test/constants/EnableConstants.java
@@ -1,16 +1,18 @@
 package com.example.commons.db.mybatis.base.sqlhelper.test.constants;
 
+import com.example.commons.constants.BaseConstants;
+import com.example.commons.utils.annotation.Constants;
+
 /**
  * @program: admin-demo-gradle
  * @description: 启用状态
  * @author: yuane
  * @create: 2020-04-06 02:47
  */
-public class EnableConstants {
+
+public class EnableConstants extends BaseConstants {
+    @Constants(name = "启用", value = EnableConstants.ENABLED)
     public static final String ENABLED = "ENABLED";
+    @Constants(name = "停用", value = EnableConstants.DISABLED)
     public static final String DISABLED = "DISABLED";
-
-    private EnableConstants() {
-
-    }
 }
