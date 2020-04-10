f22985cf80e53eef1192c41741f72cc0713acf91
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-04-10 22:01:18 +0800

    shower-application-06

---
+++ admin-demo-gradle/shower-commons-db-mybatis-mapper-base-with-sqlhelper/src/main/java/com/example/commons/db/mybatis/mapper/base/sqlhelper/test/pojo/query/query/SysUserQuery.java
@@ -0,0 +1,19 @@
+package com.example.commons.db.mybatis.mapper.base.sqlhelper.test.pojo.query.query;
+
+import com.example.commons.db.mybatis.mapper.base.sqlhelper.commons.pojo.query.CommonsQuery;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * @program: admin-demo-gradle
+ * @description:
+ * @author: yuane
+ * @create: 2020-04-06 13:26
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class SysUserQuery extends CommonsQuery {
+    private String username;
+    private String name;
+    private String enabled;
+}
