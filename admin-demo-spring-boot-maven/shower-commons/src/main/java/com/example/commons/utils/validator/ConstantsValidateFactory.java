7c1cb82f9b46d633db95bed21388fac3355ed0dd
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-04-11 21:08:53 +0800

    删除无用项目

---
+++ admin-demo-spring-boot-maven/shower-commons/src/main/java/com/example/commons/utils/validator/ConstantsValidateFactory.java
@@ -0,0 +1,27 @@
+package com.example.commons.utils.validator;
+
+import cn.hutool.core.util.ObjectUtil;
+import com.example.commons.utils.RegexUtils;
+import com.example.commons.utils.validator.annotation.ConstantsValidater;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class ConstantsValidateFactory implements ConstraintValidator<ConstantsValidater, Object> {
+    private Class<?> type;
+
+    @Override
+    public void initialize(ConstantsValidater constraintAnnotation) {
+        type = constraintAnnotation.constants();
+    }
+
+    @Override
+    public boolean isValid(Object value, ConstraintValidatorContext context) {
+        if (ObjectUtil.isEmpty(value)) {
+            return false;
+        } else {
+            String regex = RegexUtils.constantsRegex(type);
+            return value.toString().matches(regex);
+        }
+    }
+}
