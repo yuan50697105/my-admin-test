985e7f0857517e7df0afb0b90dbcccabd62684b8
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-04-18 20:14:40 +0800

    Spring Cloud

---
+++ admin-demo-spring-cloud-gradle/shower-cloud-client-011/src/main/java/com/example/cloud/service/fallback/BaseServiceFallBack.java
@@ -0,0 +1,14 @@
+package com.example.cloud.service.fallback;
+
+import com.example.cloud.service.BaseService;
+import org.springframework.stereotype.Component;
+
+/**
+ * @program: admin-demo-spring-cloud-gradle
+ * @description:
+ * @author: yuane
+ * @create: 2020-04-18 20:13
+ */
+@Component
+public class BaseServiceFallBack implements BaseService {
+}
