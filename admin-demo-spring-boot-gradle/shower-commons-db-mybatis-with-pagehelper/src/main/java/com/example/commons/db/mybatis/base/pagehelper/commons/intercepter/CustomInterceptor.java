c94595ff9de320efa37bbb59c2b922fb8ac3043a
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-04-18 12:03:10 +0800

    测试Dict注入

--- admin-demo-spring-boot-gradle/shower-commons-db-mybatis-with-pagehelper/src/main/java/com/example/commons/db/mybatis/base/pagehelper/commons/intercepter/CustomInterceptor.java
+++ admin-demo-spring-boot-gradle/shower-commons-db-mybatis-with-pagehelper/src/main/java/com/example/commons/db/mybatis/base/pagehelper/commons/intercepter/CustomInterceptor.java
@@ -9,6 +9,7 @@ import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.plugin.Intercepts;
 import org.apache.ibatis.plugin.Invocation;
 import org.apache.ibatis.plugin.Signature;
+import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
@@ -22,6 +23,7 @@ import java.util.List;
  * @author: yuane
  * @create: 2020-04-06 02:13
  */
+@Component
 @Intercepts({
         @Signature(type = Executor.class, method = "update", args = {MappedStatement.class, Object.class})
 
