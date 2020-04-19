64c747e86055d20b0dfcf03ff9295ecd0618d28c
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-04-19 22:45:27 +0800

    SpringCloudEurekaServerApplication

--- admin-demo-spring-boot-gradle/shower-application-03/src/main/java/example/ShowerApplication03.java
+++ admin-demo-spring-boot-gradle/shower-application-03/src/main/java/example/ShowerApplication03.java
@@ -10,8 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @create: 2020-04-06 13:44
  */
 @SpringBootApplication
-public class ShowerApplication02 {
+public class ShowerApplication03 {
     public static void main(String[] args) {
-        SpringApplication.run(ShowerApplication02.class, args);
+        SpringApplication.run(ShowerApplication03.class, args);
     }
 }
