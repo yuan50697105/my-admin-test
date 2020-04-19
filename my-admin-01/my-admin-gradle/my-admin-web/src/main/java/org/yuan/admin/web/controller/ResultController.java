3106ba51594dc4c50b2d46b5ae803651b1b47bbe
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-04-19 19:44:18 +0800

    my-admin-01 SysUser

--- my-admin-01/my-admin-gradle/my-admin-web/src/main/java/org/yuan/admin/web/controller/ResultController.java
+++ my-admin-01/my-admin-gradle/my-admin-web/src/main/java/org/yuan/admin/web/controller/ResultController.java
@@ -1,6 +1,7 @@
 package org.yuan.admin.web.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.yuan.admin.commons.controller.ICommonsController;
 
 /**
  * @program: my-admin-gradle
@@ -9,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
  * @create: 2020-04-19 19:34
  */
 @RestController
-public class ResultController{
+public class ResultController extends ICommonsController {
 
 }
