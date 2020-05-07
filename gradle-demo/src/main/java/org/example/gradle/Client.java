edbc1c125e7df374f617d05574e09eb897346718
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-05-07 22:43:27 +0800

    ...

---
+++ gradle-demo/src/main/java/org/example/gradle/Client.java
@@ -0,0 +1,13 @@
+package org.example.gradle;
+
+/**
+ * @program: gradle-demo
+ * @description:
+ * @author: yuane
+ * @create: 2020-05-07 22:42
+ */
+public class Client {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ maven-demo/src/main/java/org/yuan/maven/Client.java
@@ -0,0 +1,19 @@
+package org.yuan.maven;
+
+import cn.hutool.core.convert.Convert;
+import cn.hutool.core.lang.Validator;
+import cn.hutool.json.JSONUtil;
+import com.google.common.util.concurrent.RateLimiter;
+
+import java.time.Duration;
+
+/**
+ * @program: maven-demo
+ * @description:
+ * @author: yuane
+ * @create: 2020-05-07 22:29
+ */
+public class Client {
+    public static void main(String[] args) {
+    }
+}
