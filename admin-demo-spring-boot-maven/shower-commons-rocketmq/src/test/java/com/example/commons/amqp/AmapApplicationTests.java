64bc51f73711c49932165c76d5b96d8ddde00ee4
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-05-08 19:21:57 +0800

    新的spring-boot demo

---
+++ admin-demo-spring-boot-maven/shower-commons-rocketmq/src/test/java/com/example/commons/amqp/AmapApplicationTests.java
@@ -0,0 +1,9 @@
+package com.example.commons.amqp;
+
+import com.example.commons.rocketmq.RocketMqApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = RocketMqApplication.class)
+public class AmapApplicationTests {
+
+}
