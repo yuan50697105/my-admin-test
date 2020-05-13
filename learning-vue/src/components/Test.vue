d1f9702cba2382d28706a67ede88b6447944cce7
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-05-13 23:06:43 +0800

    vue 学习

--- learning-vue/src/components/Test.vue
+++ learning-vue/src/components/Test.vue
@@ -1,12 +1,34 @@
 <template>
   <div>
-<!--    <input type="text" v-model="name">-->
-<!--    {{name}}-->
-<!--    {{lastName}}-->
-<!--    {{getname}}-->
+    <!--    <input type="text" v-model="name">-->
+    <!--    {{name}}-->
+    <!--    {{lastName}}-->
+    <!--    {{getname}}-->
     <a :href="url">百度</a>
-    <div :text="1 === 1">xxxx</div>
-    
+    <div :text="1 === 1" :style="{color:'red'}">xxxx</div>
+    <div :id="name" :class="{'active':isActive}">aa</div>
+    <div v-if="isActive">
+      激活
+    </div>
+    <div v-else>
+      停用
+    </div>
+    <template v-if="isActive">
+      <div> aaaa</div>
+    </template>
+    <template v-else>
+      <div> bbbbb</div>
+    </template>
+    <template v-if="array.length>0">
+      <ul>
+        <li v-for="item in array" :key="item">
+          {{item}}
+        </li>
+      </ul>
+    </template>
+    <template v-else>
+      列表为空
+    </template>
   </div>
 </template>
 
@@ -17,7 +39,9 @@
       return {
         name: 'aaaa',
         lastName: '',
-        url:'https://www.baidu.com'
+        url: 'https://www.baidu.com',
+        isActive: false,
+        array: [1, 2, 3]
       }
     },
     watch: {
@@ -26,7 +50,7 @@
       }
     },
     computed: {
-      getname(name){
+      getname(name) {
         return name.split('').reserve().join('');
       }
     }
@@ -34,5 +58,7 @@
 </script>
 
 <style scoped>
-
+  .active {
+    color: red;
+  }
 </style>
